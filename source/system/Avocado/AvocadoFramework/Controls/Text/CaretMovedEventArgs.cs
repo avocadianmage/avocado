0699d5d1b69e7b0c07687aff5dee4fd09b27e7d1
avocadianmage <claytongotwals@gmail.com>
2015-07-20 23:32:42 -0500

    C# 6.0

--- source/system/Avocado/AvocadoFramework/Controls/Text/CaretMovedEventArgs.cs
+++ source/system/Avocado/AvocadoFramework/Controls/Text/CaretMovedEventArgs.cs
@@ -4,16 +4,13 @@ namespace AvocadoFramework.Controls.Text
 {
     sealed class CaretMovedEventArgs : EventArgs
     {
-        public TextLine ParentTextLine { get { return parentTextLine; } }
-        public int GridX { get { return gridX; } }
-
-        readonly TextLine parentTextLine;
-        readonly int gridX;
+        public TextLine ParentTextLine { get; }
+        public int GridX { get; }
 
         public CaretMovedEventArgs(TextLine parentTextLine, int gridX)
         {
-            this.parentTextLine = parentTextLine;
-            this.gridX = gridX;
+            ParentTextLine = parentTextLine;
+            GridX = gridX;
         }
     }
 }
