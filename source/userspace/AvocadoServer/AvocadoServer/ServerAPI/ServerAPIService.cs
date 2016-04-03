5a12ca4117418c731a7942e8767cbdc7e7d38ce1
avocadianmage <claytongotwals@gmail.com>
2016-04-02 19:54:12 -0500

    refactored service logic to new file

--- source/userspace/AvocadoServer/AvocadoServer/ServerAPI/ServerAPIService.cs
+++ source/userspace/AvocadoServer/AvocadoServer/ServerAPI/ServerAPIService.cs
@@ -1,8 +1,8 @@
 ﻿using AvocadoServer.ServerCore;
-using System;
 using System.Collections.Generic;
 using System.Reflection;
 using System.ServiceModel;
+using static AvocadoServer.ServerAPI.ServiceLogic;
 
 namespace AvocadoServer.ServerAPI
 {
@@ -15,18 +15,15 @@ namespace AvocadoServer.ServerAPI
         [AllowedClient(ClientType.LAN)]
         public Pipeline<IEnumerable<string>> GetJobs()
         {
-            return executeRequest(
+            return ExecuteRequest(
                 p => EntryPoint.Jobs.GetJobTableInfo(), 
                 MethodBase.GetCurrentMethod());
         }
 
         [OperationBehavior(Impersonation = ImpersonationOption.Allowed)]
-        public Pipeline RunJob(
-            string filename, 
-            int secInterval, 
-            string[] args)
+        public Pipeline RunJob(string filename, int secInterval, string[] args)
         {
-            return executeRequest(
+            return ExecuteRequest(
                 p => EntryPoint.Jobs.StartJob(p, filename, secInterval, args), 
                 MethodBase.GetCurrentMethod(), 
                 filename, secInterval, args);
@@ -36,39 +33,9 @@ namespace AvocadoServer.ServerAPI
         [AllowedClient(ClientType.LAN)]
         public Pipeline KillJob(int id)
         {
-            return executeRequest(
+            return ExecuteRequest(
                 p => EntryPoint.Jobs.KillJob(p, id),
                 MethodBase.GetCurrentMethod(), id);
-        }
-
-        Pipeline executeRequest(
-            Action<Pipeline> action, MethodBase method, params object[] args)
-        {
-            var result = executeRequest(
-                p => { action(p); return default(int); },
-                method,
-                args);
-            return new Pipeline
-            {
-                Success = result.Success,
-                Message = result.Message
-            };
-        }
-
-        Pipeline<T> executeRequest<T>(
-            Func<Pipeline, T> func, MethodBase method, params object[] args)
-        {
-            var pipeline = new Pipeline<T>();
-            CommandContext.RunChecks(pipeline, method, args);
-
-            try
-            {
-                if (!pipeline.Success) return pipeline;
-
-                pipeline.Data = func(pipeline);
-                return pipeline;
-            }
-            finally { pipeline.Log(); }
         }
     }
 }
