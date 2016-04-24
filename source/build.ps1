d0e1f2aaf9c0b771fa96388a4f1901b827ff4608
avocadianmage <claytongotwals@gmail.com>
2016-04-24 03:20:33 -0500

    use correct msbuild

--- source/build.ps1
+++ source/build.ps1
@@ -1,6 +1,6 @@
 function build($path) 
 { 
-    C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe $path /t:Build /p:Configuration=Release 
+    & "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" $path /t:Build /p:Configuration=Release
 }
 
 # Build system solutions.
