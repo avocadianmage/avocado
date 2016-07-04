dbee4af0875a81b9f3f0967b42f9c1401461db5c
avocadianmage <claytongotwals@gmail.com>
2016-07-03 21:16:15 -0500

    reorganize powershell code

--- source/userspace/AvocadoShell/AvocadoShell/Scripts/core.ps1
+++ source/userspace/AvocadoShell/AvocadoShell/Scripts/core.ps1
@@ -1,19 +1,8 @@
 # Import formatting file.
 Update-FormatData -PrependPath $PSHOME\Avocado.format.ps1xml
 
-# Import the user profile.
+# Import user profile.
 $PROFILE = Join-Path `
 	([Environment]::GetFolderPath("MyDocuments")) `
 	"WindowsPowerShell\profile.ps1"
 if (Test-Path $PROFILE) { . $PROFILE }
-
-# Communicate back to the core host (C# code).
-function RunNativeCommand
-{
-	Param([Parameter(Mandatory = $true)][string]$Command)
-
-	Write-Verbose "avocado:$Command" -Verbose
-}
-
-# Returns true if PowerShell code is executing in a remote session.
-function IsRemoteSession { (Get-Host).Name -eq "ServerRemoteHost" }
--- source/userspace/AvocadoShell/AvocadoShell/Scripts/utilities.ps1
+++ source/userspace/AvocadoShell/AvocadoShell/Scripts/utilities.ps1
@@ -1,3 +1,14 @@
+# Communicate back to the core host (C# code).
+function RunNativeCommand
+{
+	Param([Parameter(Mandatory = $true)][string]$Command)
+
+	Write-Verbose "avocado:$Command" -Verbose
+}
+
+# Returns true if PowerShell code is executing in a remote session.
+function IsRemoteSession { (Get-Host).Name -eq "ServerRemoteHost" }
+
 # Utility function executed by local PowerShell instance to complete the remote
 # download.
 function SendToLocal
