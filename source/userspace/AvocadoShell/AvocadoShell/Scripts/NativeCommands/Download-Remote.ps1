066d4c09959bb1efdf3a92f28deb2ff8e8583424
avocadianmage <claytongotwals@gmail.com>
2016-08-31 21:05:44 -0500

    support array of files for rdl

--- source/userspace/AvocadoShell/AvocadoShell/Scripts/NativeCommands/Download-Remote.ps1
+++ source/userspace/AvocadoShell/AvocadoShell/Scripts/NativeCommands/Download-Remote.ps1
@@ -1,21 +1,16 @@
 # Download a file from a remote PowerShell session.
 function Download-Remote
 {
-	Param([Parameter(Mandatory = $true)][string]$Path)
+	Param([Parameter(Mandatory = $true)][string[]]$Path)
 
 	# Throw error if this was called from a remote session.
 	if (-not (IsRemoteSession)) { Throw "No remote session found." }
 
-	# Verify the file exists.
-	$fullPaths = Get-Item $Path
-	if (-not $?) { Return }
-
-	# Format path, handling multiple.
-	$pathList = $fullPaths -join "`",`""
-	$pathList = "`"$pathList`""
+	# Get full paths.
+	$Path = ($Path | foreach { Get-Item $_ }) -join ","
 
 	# Run native command.
-	RunNativeCommand "Download-Remote $pathList"
+	RunNativeCommand "Download-Remote $Path"
 }
 
 # Shortcut.
