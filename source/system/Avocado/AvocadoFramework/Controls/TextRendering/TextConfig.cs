d782bd5d3297435ba49ab648ccb46357fce46bde
avocadianmage <claytongotwals@gmail.com>
2016-11-20 16:41:32 -0600

    consolidate text settings

---
+++ source/system/Avocado/AvocadoFramework/Controls/TextRendering/TextConfig.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace AvocadoFramework.Controls.TextRendering
+{
+    static class TextConfig
+    {
+        public static FontFamily FontFamily { get; } 
+            = new FontFamily("Consolas");
+        public static double FontSize => 9.5;
+        public static FontWeight FontWeight => FontWeights.Bold;
+        public static TextFormattingMode TextFormattingMode 
+            => TextFormattingMode.Display;
+    }
+}
