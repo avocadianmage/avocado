109cb12e9ec4f585a0335933229f127a28751c04
avocadianmage <claytongotwals@gmail.com>
2016-12-30 23:26:44 -0600

    slight refactor of brush

--- source/userspace/AvocadoShell/AvocadoShell/Config.cs
+++ source/userspace/AvocadoShell/AvocadoShell/Config.cs
@@ -12,7 +12,8 @@ namespace AvocadoShell
         public static Brush ErrorFontBrush => Brushes.Salmon;
         public static Brush SystemFontBrush => Brushes.LightGray;
 
-        public static Brush InputBackgroundBrush { get; }
+        public static Brush InputBackgroundBrush { get; } 
+            = new SolidColorBrush(Colors.Gray) { Opacity = 0.25, };
 
         public static ConsoleColor SystemConsoleForeground 
             => ConsoleColor.Gray;
@@ -42,10 +43,6 @@ namespace AvocadoShell
 
         static Config()
         {
-            InputBackgroundBrush = new SolidColorBrush(Colors.Gray)
-            {
-                Opacity = 0.25,
-            };
             InputBackgroundBrush.Freeze();
         }
     }
