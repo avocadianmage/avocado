1369ea17df33dd4ee0a40fe4eed81b73faf7b94d
avocadianmage <claytongotwals@gmail.com>
2017-01-01 17:41:17 -0600

    move downloader to textpalette

--- source/userspace/AvocadoDownloader/AvocadoDownloader/Config.cs
+++ source/userspace/AvocadoDownloader/AvocadoDownloader/Config.cs
@@ -1,9 +1,15 @@
-﻿namespace AvocadoDownloader
+﻿using AvocadoFramework.Controls.TextRendering;
+using System.Windows.Media;
+
+namespace AvocadoDownloader
 {
     static class Config
     {
         public static string MutexName => "AvocadoDownloaderMutex";
 
         public static string StartDownloadStatus => "Starting download...";
+
+        public static Color ActiveGrouperColor => TextPalette.Orange.Color;
+        public static Color InactiveGrouperColor => TextPalette.LightGreen.Color;
     }
 }
