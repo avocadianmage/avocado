09df120850d712215e5be0256ea7350957a1a949
avocadianmage <claytongotwals@gmail.com>
2017-04-29 15:18:24 -0500

    registry lib function

---
+++ source/system/StandardLibrary/StandardLibrary/Utilities/SystemUtils.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Win32;
+
+namespace StandardLibrary.Utilities
+{
+    public static class SystemUtils
+    {
+        public static void SetRegistryValue(
+            string keyPath, string valueName, object valueData)
+        {
+            Registry.CurrentUser
+                .OpenSubKey(keyPath, true)
+                .SetValue(valueName, valueData);
+        }
+    }
+}
